adbb9b8132bfc22add78ab9e418606b62dc57cbd
Vlad Sarpe <vsarpe@gmail.com>
2019-11-26 13:35:22 -0700

    Add DB file name mapping and file unlink

--- index.js
+++ index.js
@@ -2,30 +2,33 @@
 
 require("dotenv").config();
 
-const Fs = require('fs');
-const Util = require('util');
-const Path = require('path');
-const AWSTools = require('./lib/aws-tools');
+const fs = require('fs');
+const util = require('util');
+const path = require('path');
+const AWSManager = require('./lib/aws-manager');
+const DBManager = require('./lib/db-manager');
 
+const DB_FILE = process.env.DB_FILE;
 const ROOT_FOLDER = process.env.ROOT_FOLDER;
 const HOURS_THRESHOLD = process.env.FILE_AGE_THRESHOLD_HOURS;
 const HOURS_PER_MS = 36e5;
 
-const fsStat = Util.promisify(Fs.stat);
+const fsStat = util.promisify(fs.stat);
 
 const handleFile = async (file) => {
-  console.log(file);
-
   try {
     const stats = await fsStat(file);
     const hours = Math.abs(new Date() - stats.birthtime) / HOURS_PER_MS;
     if (hours > HOURS_THRESHOLD) {
       // Flip Windows slashes
       const key = file.split(ROOT_FOLDER).join('').split('\\').join('/');
-      console.log(key);
-
-      const body = Fs.readFileSync(file);
-      await AWSTools.upload(key, body);
+      const body = fs.readFileSync(file);
+      const result = await AWSManager.upload(key, body);
+      const insert = await DBManager.run(`INSERT INTO files(original_path, s3_location, s3_bucket, s3_key) VALUES ('${file}', '${result.Location}', '${result.Bucket}', '${result.Key}')`);
+      if (insert) {
+        // Delete file after successful download
+        fs.unlinkSync(file);
+      }
     }
   } catch (error) {
     // Skip failed files, they will be handled on a subsequent run
@@ -35,13 +38,13 @@ const handleFile = async (file) => {
 
 const handleDirectory = async (dirPath) => {
   try {
-    const contents = Fs.readdirSync(dirPath);
+    const contents = fs.readdirSync(dirPath);
     for (const content of contents) {
-      const path = Path.join(dirPath, content);
-      if (Fs.statSync(path).isFile()) {
-        await handleFile(path);
+      const fullPath = path.join(dirPath, content);
+      if (fs.statSync(fullPath).isFile()) {
+        await handleFile(fullPath);
       } else {
-        await handleDirectory(path);
+        await handleDirectory(fullPath);
       }
     }
   } catch (error) {
@@ -53,7 +56,10 @@ const handleDirectory = async (dirPath) => {
 const start = async () => {
   console.log(`Archive started '${ROOT_FOLDER}'`);
 
+  await DBManager.open(DB_FILE);
+  await DBManager.initialize();
   await handleDirectory(ROOT_FOLDER);
+  await DBManager.close();
 
   console.log(`Archive finished '${ROOT_FOLDER}'`);
 };
